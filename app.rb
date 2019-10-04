45565974a191a979b4aacbfba7614162274bc2ef
sami suliman <sami.bco@gmail.com>
2019-10-04 03:39:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  
+      get '/' do
+        "Hello, World!"
+      end
+    
+      get '/name' do
+        "My name is Sam"
+      end
+    
+      get '/hometown' do
+        "My hometown is Texas"
+      end
+    
+      get '/favorite-song' do
+        "My favorite song is 'Doesn't matter'"
+      end
 end
